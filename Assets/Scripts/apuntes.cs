3dc74b5cdb68015b8f91d314a919631e9cae5e09
adelamarinmr <adelamarinmr@gmail.com>
2024-09-26 20:52:10 +0200

    Update apuntes.cs

--- Assets/Scripts/apuntes.cs
+++ Assets/Scripts/apuntes.cs
@@ -7,7 +7,12 @@ public class apuntes : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //transform.position = new Vector3(3, 5, 0);
+        //transform.eulerAngles = new Vector3(3, 4, 7);
+        //transform.localScale = new Vector3(1, 1, 1);
+
+        //transform.rotation = Quaternion.identity; NO
+
     }
 
     // Update is called once per frame
