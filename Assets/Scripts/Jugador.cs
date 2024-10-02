5aa731936d8b483256322f11865c95e7a9265106
adelamarinmr <adelamarinmr@gmail.com>
2024-10-02 20:13:28 +0200

    lloro

--- Assets/Scripts/Jugador.cs
+++ Assets/Scripts/Jugador.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Jugador : MonoBehaviour
 {
+    [SerializeField] float x;
+    [SerializeField] float z;
+    [SerializeField] float speed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,21 @@ public class Jugador : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+     
     }
+
+    void Mover(float h, float v)
+    {
+        transform.Translate( new Vector3(h, v));
+        Mover(h, v);
+
+        float speed = Input.GetAxisRaw("Horizontal");
+        transform.Translate( new Vector3(speed, h));
+
+    }
+
+
+
 }
