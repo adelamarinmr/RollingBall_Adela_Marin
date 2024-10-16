4172706bb7e4f9eb8b7f5a4b9820944d0025df6b
adelamarinmr <adelamarinmr@gmail.com>
2024-10-16 19:27:56 +0200

    hoy devoré

--- Assets/Scripts/Pared.cs
+++ Assets/Scripts/Pared.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Pared : MonoBehaviour
 {
+
+    [SerializeField] private Rigidbody[] rbs;
+    private float timer = 0f;
+    private bool iniciarCuenta = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -11,16 +16,31 @@ public class Pared : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         
+
+        if (iniciarCuenta )
+        {
+            timer += 1 * Time.unscaledDeltaTime;
+            if ( timer >= 2)
+            {
+                Time.timeScale = 1f;
+                for ( int i = 0; i < rbs.Length; i++ )
+                {
+                    rbs[i].useGravity = true;
+                }
+            }
+
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            Time.timeScale = 0.3f;
+            Time.timeScale = 0.2f;
+            iniciarCuenta |= true;
         }
     }
 
