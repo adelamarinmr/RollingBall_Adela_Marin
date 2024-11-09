1673800f3699b6680df570ba7552530f45dae826
adelamarinmr <adelamarinmr@gmail.com>
2024-11-09 16:23:48 +0100

    NO SE

--- Assets/Scripts/Coleccionable.cs
+++ Assets/Scripts/Coleccionable.cs
@@ -5,22 +5,14 @@ using UnityEngine;
 public class Coleccionable : MonoBehaviour
 {
     // Start is called before the first frame update
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.transform.CompareTag("Player"))
-        {
-            FindAnyObjectByType<GM>().AddColeccionable(); // llama al metdo y destruye
-
-            Destroy(gameObject);
-        }
-    }
+ 
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Coleccionable"))
+        if (other.gameObject.CompareTag("Player"))
         {
-            manager.ReproducirSonido(magicSound);
-            Destroy(other.gameObject);
+            FindAnyObjectByType<GM>().AddColeccionable();
+            Destroy(gameObject);
         }
 
 
